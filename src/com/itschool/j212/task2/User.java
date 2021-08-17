f1b53ff6b303dc64cd8a0889d1fcf64321304e28
it-school <GITHUB it@osvita.od.ua>
2021-08-17 21:32:44 +0300

    Update for j212

--- src/com/itschool/j212/task2/User.java
+++ src/com/itschool/j212/task2/User.java
@@ -1,17 +1,57 @@
 package com.itschool.j212.task2;
 
 public class User {
-   int ID;
-   String login;
-   String password;
-   String name;
-   boolean isDeleted;
+   private int ID;
+   private String login;
+   private String password;
+   private String name;
+   private boolean isDeleted;
 
    public User(String login, String password, String name) {
       this.login = login;
       this.password = password;
       this.name = name;
       this.isDeleted = false;
+   }
+
+   public int getID() {
+      return ID;
+   }
+
+   public void setID(int ID) {
+      this.ID = ID;
+   }
+
+   public String getLogin() {
+      return login;
+   }
+
+   public void setLogin(String login) {
+      this.login = login;
+   }
+
+   public String getPassword() {
+      return password;
+   }
+
+   public void setPassword(String password) {
+      this.password = password;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public boolean isDeleted() {
+      return isDeleted;
+   }
+
+   public void setDeleted(boolean deleted) {
+      isDeleted = deleted;
    }
 
    @Override
